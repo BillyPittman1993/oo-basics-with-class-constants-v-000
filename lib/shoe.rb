2eff367f55593837c430521a6748f68f26a0795b
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-28 19:18:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.include?(brand)
+    else 
+      BRANDS << brand
+    end
   end
-  
-  brand.each do |brand| 
-    if brand.uniq? 
-  
 end
